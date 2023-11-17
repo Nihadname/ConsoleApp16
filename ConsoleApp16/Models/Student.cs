206442e5b01846e9fdef08a4ec61509e3eb7558f
Nihadname <nihadcoding@gmail.com>
2023-11-17 21:08:55 +0400

    Add project files.

---
+++ ConsoleApp16/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16.Models
+{
+    internal class Student
+    {
+        public string Name;
+       public string SurName;
+        public int age;
+        public string address;
+
+        public Student() {
+        
+        
+        }
+        public Student(string name, string surName, int age)
+        {
+            this.Name = name;
+            this.SurName = surName;
+            this.age = age;
+        }
+        public Student(string name, string surName, int age, string address):this() 
+        {
+            this.Name = name;
+            this.SurName = surName;
+            this.age = age;
+            this.address = address;
+        }
+    }
+}
